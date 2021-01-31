8582f72eb692cbc9bb436ee55d4cfacc7d23137e
Shawn Van Every <Shawn.Van.Every@nyu.edu>
2021-01-31 14:38:18 -0500

    First commit of the files

---
+++ server.js
@@ -0,0 +1,13 @@
+// Express is a node module for building HTTP servers
+var express = require('express');
+var app = express();
+
+// Tell Express to look in the "public" director for any files, first!
+app.use(express.static("public"));
+
+// The default route of / and what to do!
+app.get("/", function(req, res) {
+    res.send("<html><body><h1>Hello Hello Thank you for connecting!</h1></body></html>");
+});
+
+app.listen(80);
